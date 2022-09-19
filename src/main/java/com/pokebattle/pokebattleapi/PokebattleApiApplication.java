550ddbc6e38e4912b7a338cdba683b12128690d4
F9329128 Thiago Henrique Assi <thiago.assi@bb.com.br>
2022-09-19 14:22:23 -0300

    generated initial spring files

---
+++ src/main/java/com/pokebattle/pokebattleapi/PokebattleApiApplication.java
@@ -0,0 +1,13 @@
+package com.pokebattle.pokebattleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokebattleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokebattleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokebattle/pokebattleapi/PokebattleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokebattle.pokebattleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokebattleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
