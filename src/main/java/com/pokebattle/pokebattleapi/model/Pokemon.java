57db17034afb4d94e66291ced82bdf7acd748102
Thiago <thiago-assi@outlook.com>
2022-09-26 23:31:16 -0300

    fixed unnecessary @ManyToMany relationship

--- src/main/java/com/pokebattle/pokebattleapi/model/Pokemon.java
+++ src/main/java/com/pokebattle/pokebattleapi/model/Pokemon.java
@@ -1,13 +1,10 @@
 package com.pokebattle.pokebattleapi.model;
 
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -33,9 +30,6 @@ public class Pokemon {
 
     @NotBlank
     private String name;
-    
-    @ManyToMany(mappedBy = "pokemons")
-    private List<User> users;
     
     @OneToOne(cascade = CascadeType.ALL, optional = false)
     @NotNull
