81eb125c6cfec37ba5b847d738e8ce2682389921
F9329128 Thiago Henrique Assi <thiago.assi@bb.com.br>
2022-09-29 16:10:44 -0300

    refactored exceptions format

--- src/main/java/com/pokebattle/pokebattleapi/exceptions/pokebattle/PokebattleException.java
+++ src/main/java/com/pokebattle/pokebattleapi/exceptions/pokebattle/PokebattleException.java
@@ -1,5 +1,7 @@
 package com.pokebattle.pokebattleapi.exceptions.pokebattle;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 
 @Getter
@@ -8,9 +10,11 @@ public class PokebattleException extends RuntimeException {
     public PokebattleException(PokebattleExceptionCodes pokebattleExceptionCodes) {
         this.code = pokebattleExceptionCodes.getCode();
         this.message = pokebattleExceptionCodes.getMessage();
+        this.httpStatus = pokebattleExceptionCodes.getHttpStatus();
     }
     
     private Integer code;
     private String message;
+    private HttpStatus httpStatus;
 
 }
--- src/main/java/com/pokebattle/pokebattleapi/exceptions/pokebattle/PokebattleExceptionCodes.java
+++ src/main/java/com/pokebattle/pokebattleapi/exceptions/pokebattle/PokebattleExceptionCodes.java
@@ -1,19 +1,23 @@
 package com.pokebattle.pokebattleapi.exceptions.pokebattle;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 
 @Getter
 public enum PokebattleExceptionCodes {
-    USER_NOT_FOUND(101, "User not found"),
-    USER_ALREADY_EXISTS(102, "Username already registered"),
-    INVALID_POKEMON_IDENTIFIER(201, "Invalid pokémon name/id");
+    USER_NOT_FOUND(101, "User not found", HttpStatus.NOT_FOUND),
+    USER_ALREADY_EXISTS(102, "Username already registered", HttpStatus.CONFLICT),
+    INVALID_POKEMON_IDENTIFIER(201, "Invalid pokémon name/id", HttpStatus.NOT_FOUND);
 
     private Integer code;
     private String message;
+    private HttpStatus httpStatus;
 
-    PokebattleExceptionCodes(Integer code, String message) {
+    PokebattleExceptionCodes(Integer code, String message, HttpStatus httpStatus) {
         this.code = code;
         this.message = message;
+        this.httpStatus = httpStatus;
     }
 
 }
