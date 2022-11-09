06510dea563b69679ed5a84a45b56e237ffd5b04
Thiago <thiago-assi@outlook.com>
2022-11-09 02:42:21 -0300

    refactored exceptions format

--- src/main/java/com/pokebattle/pokebattleapi/exceptions/pokebattle/PokebattleExceptionHandler.java
+++ src/main/java/com/pokebattle/pokebattleapi/exceptions/pokebattle/PokebattleExceptionHandler.java
@@ -1,22 +1,20 @@
 package com.pokebattle.pokebattleapi.exceptions.pokebattle;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class PokebattleExceptionHandler {
 
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(PokebattleException.class)
-    public PokebattleExceptionDto handle(PokebattleException exception) {
+    public ResponseEntity<PokebattleExceptionDto> handle(PokebattleException exception) {
         String message = exception.getMessage();
         Integer code = exception.getCode();
 
         PokebattleExceptionDto dto = new PokebattleExceptionDto(message, code);
 
-        return dto;
+        return ResponseEntity.status(exception.getHttpStatus()).body(dto);
     }
 
 }
